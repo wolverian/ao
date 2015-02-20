e3297d034e5b6ae0a522c985f210d3be6d83a42d
Ilmari Vacklin <ilmari.vacklin@reaktor.fi>
2015-02-20 14:46:10 +0200

    Fix import paths

--- main.go
+++ main.go
@@ -18,9 +18,9 @@ import (
 	"strconv"
 	"strings"
 
-	"code.google.com/p/goplan9/plan9"
-	"code.google.com/p/goplan9/plan9/acme"
-	"code.google.com/p/goplan9/plan9/client"
+	"9fans.net/go/plan9"
+	"9fans.net/go/acme"
+	"9fans.net/go/plan9/client"
 
 	"code.google.com/p/go.tools/go/loader"
 	"code.google.com/p/go.tools/oracle"
